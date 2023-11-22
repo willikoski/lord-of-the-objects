5af072e46b51b601864e3774a3935c5cd13b1d24
unknown <wkostreski@gmail.com>
2023-11-21 22:10:08 -0500

    Chapter 3

--- js/app.js
+++ js/app.js
@@ -103,10 +103,14 @@ function keepItSecretKeepItSafe() {
   console.log("3: keepItSecretKeepItSafe");
 
   // create a div with an id of `'the-ring'`
+  const ringContainer = document.createElement("div");
+  ringContainer.setAttribute("id", "the-ring");
 
   // give the div a class of `'magic-imbued-jewelry'`
+  ringContainer.classList.add("magic-imbued-jewelry");
 
   // add the ring as a child of `Frodo`
+  document.querySelectorAll(".hobbit")[0].appendChild(ringContainer);
 
 }
 
